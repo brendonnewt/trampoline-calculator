59a5a6f34d1041950322250d28899b604fd12f45
Brendon Newton <brendonnewton0@gmail.com>
2024-03-09 23:19:55 -0600

    Cookies system fully works, passes can be saved and used for calculations

--- static/menu.js
+++ static/menu.js
@@ -8,79 +8,138 @@
 Description: This file contains the menu javascript functions for calculator.html
 
 */
+
+/* Loads all previously saved combinations */
 function loadCombos() {
+
+    //Gets all combination names
     var combinations = getCookie('combinations');
     combinations = combinations ? combinations.split(',') : [];
+
+    //Creates a button for each combination
     for (var i = 0; i < combinations.length; i++) {
+        var menu = document.getElementById('menu');
+
+        //Creates the button
         var newButton = document.createElement('button');
         newButton.textContent = combinations[i];
-        newButton.onclick = function() {
-            var combo = getCookie(this.textContent).split('=')[1];
+
+        //Adds functionality to the button
+        newButton.onclick = function(event) {
+            event.preventDefault();
+            var combo = getCookie(this.textContent);
+            combo = combo.replace(/,/g, ' + ');
             document.getElementById('hidden-result').value = combo.replace(/•/g, 'o');
             document.getElementById('result-value').value = combo;
+            closeMenu();
         };
+
+        //Adds the button to the menu
         document.getElementById('menu').appendChild(newButton);
     }
+
 };
 
 /* Toggles the visibility of the menu */
 function toggleMenu() {
     var menu = document.getElementById('menu');
+    //Shows the menu
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         menu.classList.add('flex');
         document.getElementById('menu-header').classList.add('flex');
         loadCombos();
+        refreshInfo();
+    //Hides the menu
     } else {
-        menu.classList.add('hidden');
-        menu.classList.remove('flex');
+        closeMenu();
     }
 };
 
+/* Closes the menu */
 function closeMenu() {
+    //Hides the menu
     var menu = document.getElementById('menu');
     menu.classList.add('hidden');
     menu.classList.remove('flex');
+
+    //Gets list of combinations
+    var combinations = getCookie('combinations');
+    combinations = combinations ? combinations.split(',') : [];
+
+    //Removes all buttons from the menu
+    for (var i = 0; i < combinations.length; i++) {
+        document.getElementById('menu').lastChild.remove();
+    }
 };
 
+/* Refreshes the input fields */
 function refreshInfo() {
-    document.getElementById('')
+    //Resets the input fields
+    document.getElementById('name-input').value = "";
+    document.getElementById('combo-input').value = "";
+    document.getElementById('hidden-combo').value = "";
+    document.getElementById('remove-input').value = "";
+    document.getElementById('name-head').innerHTML = "Enter a name for your combination";
+    document.getElementById('combo-head').innerHTML = "Add a skill to your combination";
+    document.getElementById('remove-head').innerHTML = "Enter the name of the combination to remove";
 }
 
 /* Adds a combination to the menu, goes to submitName() */
 function addCombination() {
     closeMenu();
+    //Shows the name prompt
     document.getElementById('name-prompt').classList.remove('hidden');
     document.getElementById('name-prompt').classList.add('flex');
 }
 
+/* Removes a combination from the menu */
 function removeCombination() {
     closeMenu();
+    //Shows the remove prompt
     document.getElementById('remove-prompt').classList.remove('hidden');
     document.getElementById('remove-prompt').classList.add('flex');
 }
 
+/* Removes the cookie for the combination */
 function submitRemove() {
+    //Gets the name to be removed
     var name = document.getElementById('remove-input').value;
     var combinations = getCookie('combinations').split(',');
     var index = combinations.indexOf(name);
+
+    if (getCookie(name) == "") {
+        document.getElementById('remove-head').innerHTML = "No combination with that name exists";
+        return;
+    }
+
+    //Checks if the name is valid and removes it from combinations list
     if (index > -1) {
         combinations.splice(index, 1);
     }
+
+    //Resets combinations cookie and removes the combination cookie
     setCookie('combinations', combinations, 30);
     setCookie(name, "", 0);
+
+    //Hides the remove prompt
     document.getElementById('remove-prompt').classList.add('hidden');
     document.getElementById('remove-prompt').classList.remove('flex');
+
+    //Refreshes the page
     document.getElementById('calculator-form').submit();
 }
 
+/* Cancels the remove prompt */
 function cancelRemove() {
+    //Hides the remove prompt
     document.getElementById('remove-prompt').classList.add('hidden');
     document.getElementById('remove-prompt').classList.remove('flex');
 }
 
 /* Submits the name for the combination, goes to submitSkill() */
 function submitName() {
+    //Checks if the name is empty
     if (document.getElementById('name-input').value == "") {
         document.getElementById('name-head').innerHTML = "Please enter a name";
         return;
@@ -88,23 +147,36 @@ function submitName() {
         document.getElementById('name-head').innerHTML = "Name your combination";
     }
 
-    /* TODO: Add a check for if the name is already taken */
+    var name = document.getElementById('name-input').value;
 
+    //Checks if the name is already taken
+    var combinations = getCookie('combinations').split(',');
+    if (combinations.includes(name)) {
+        document.getElementById('name-head').innerHTML = "Name is already taken";
+        return;
+    }
+
+    //Hides the name prompt
     document.getElementById('name-prompt').classList.add('hidden');
     document.getElementById('name-prompt').classList.remove('flex');
 
+    //Shows the combo prompt
     document.getElementById('combo-prompt').classList.remove('hidden');
     document.getElementById('combo-prompt').classList.add('flex');
 }
 
 function cancelName() {
+    //Resets the input field
     document.getElementById('name-input').value = "";
+
+    //Hides the name prompt
     document.getElementById('name-prompt').classList.add('hidden');
     document.getElementById('name-prompt').classList.remove('flex');
 }
 
 /* Submits a skill to the combination, goes to submitCombo() */
 function submitSkill() {
+    //Checks if the skill is empty
     if (document.getElementById('combo-input').value == "") {
         document.getElementById('combo-head').innerHTML = "Please enter a skill";
         return;
@@ -112,43 +184,49 @@ function submitSkill() {
         document.getElementById('combo-head').innerHTML = "Add a skill to your combination";
     }
 
+    //Adds the skill to the hidden combo
     var hiddenCombo = document.getElementById('hidden-combo');
     var skill = document.getElementById('combo-input').value;
+
+    //Formats the skill
     if (hiddenCombo.value == "") {hiddenCombo.value = skill}
     else {hiddenCombo.value += "," + skill;}
+
+    //Resets the input field
     document.getElementById('combo-input').value = "";
 }
 
 /* Submits the combination to the menu, goes to submitAdd() */
 function submitCombo() {
-
+    //Hides the combo prompt
     document.getElementById('combo-prompt').classList.remove('flex');
     document.getElementById('combo-prompt').classList.add('hidden');
     
-
+    //Sets the name and value of the combination
     var nameDisplay = document.getElementById('name-input');
     document.getElementById('combo-name').value = "Name: " + nameDisplay.value;
-
     var hiddenCombo = document.getElementById('hidden-combo');
     document.getElementById('combo-value').value = hiddenCombo.value;
 
+    //Shows the add prompt
     document.getElementById('add-prompt').classList.remove('hidden');
     document.getElementById('add-prompt').classList.add('flex');
 }
 
 function cancelAdd() {
-    document.getElementById('hidden-combo').value = "";
-    document.getElementById('combo-input').value = "";
-    document.getElementById('name-prompt').value = "";
+    //Hides the add prompt
     document.getElementById('add-prompt').classList.add('hidden');
     document.getElementById('add-prompt').classList.remove('flex');
 }
 
 /* Makes a cookie for the combination */
 function submitAdd() {
+    //Gets the name and value of the combination
     var name = document.getElementById('name-input').value;
     var combo = document.getElementById('hidden-combo').value;
     setCookie(name, combo, 30);
+
+    //Adds the name to the combinations list
     var combinations = getCookie('combinations');
     if (combinations) {
         combinations += ',' + name;
@@ -157,11 +235,13 @@ function submitAdd() {
     }
     setCookie('combinations', combinations, 30);
 
+    //Hides the add prompt and refreshes the page
     document.getElementById('add-prompt').classList.add('hidden');
     document.getElementById('add-prompt').classList.remove('flex');
+    document.getElementById('calculator-form').submit();
 }
 
-/* Sets a coockie for a combination */
+/* Sets a cookie for a combination */
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -183,14 +263,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-
-/* Checks if a combination has a cookie */
-function checkCookie() {
-    let combo = getCookie("name");
-    if (combo != "") {
-        //Modify this to return the result of the combo's sum
-    } else {
-        //TODO: Display combo name is invalid
-    }
 }
